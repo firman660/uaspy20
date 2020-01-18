dcd3f3b470b884c696dea0f7efbd21e123a1bbf8
firman Syah <56372587+firman660@users.noreply.github.com>
2020-01-18 08:57:04 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-class MainApp():
+from view.view_book import *
+from view.input_book import *
+from core.baseapp import BaseApp
+from core.search_helper import SearchHelper
+
+class MainApp(BaseApp):
     def __init__(self):
         self.books = []
+def list_book(self):
+    ViewBook.list(self)
 
+def add_book(self):
+    InputBook.input(self)
+    SearchHelper.search_title(self)
 
 if __name__ == "__main__":
     app = MainApp()
